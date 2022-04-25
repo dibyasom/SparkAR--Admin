389c9a8b0a9f28cbe13c3769ab656a8e3fa34b88
Dibyasom <dibya.secret@gmail.com>
2022-04-25 12:31:43 +0530

    PROD config added

--- core/admin.py
+++ core/admin.py
@@ -4,6 +4,7 @@ from core.models import Applicant
 from django.db.models.functions import Lower, Upper
 from import_export.admin import ExportActionMixin
 
+
 @admin.register(Applicant)
 class UniversalAdmin(ExportActionMixin, admin.ModelAdmin):
     def get_list_display(self, request):
@@ -11,3 +12,6 @@ class UniversalAdmin(ExportActionMixin, admin.ModelAdmin):
 
     def get_ordering(self, request):
         return [Upper("fbUrl")]  # sort case insensitive
+
+    def get_search_fields(self, request):
+        return ("email",)
